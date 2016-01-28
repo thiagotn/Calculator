082e101ae18671ea9eca56096c85dd535eaed56a
Usuário Convidado <Guest@L2602MICRO12.local>
2016-01-28 20:24:01 -0200

    Primeiro commit

---
+++ Calculator/CalcOperation.swift
@@ -0,0 +1,13 @@
+//
+//  CalcOperation.swift
+//  Calculator
+//
+//  Created by Usuário Convidado on 28/01/16.
+//  Copyright © 2016 Usuário Convidado. All rights reserved.
+//
+
+import UIKit
+
+class CalcOperation: NSObject {
+
+}
